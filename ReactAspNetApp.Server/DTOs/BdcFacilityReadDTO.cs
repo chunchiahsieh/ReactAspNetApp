ea3df278aed8ab1a605b4287a84e8b0166b2f83a
kkggkkss <chunchiahsieh2@gmail.com>
2024-05-13 13:59:26 +0800

    DTO

---
+++ ReactAspNetApp.Server/DTOs/BdcFacilityReadDTO.cs
@@ -0,0 +1,74 @@
+﻿using AutoMapper;
+
+namespace ReactAspNetApp.Server.Models;
+
+public partial class BdcFacilityReadDTO
+{
+    public int FacilityId { get; set; }
+
+    public int OrgGroupId { get; set; }
+
+    public int OrgUnitId { get; set; }
+
+    public string FacilityName { get; set; } = null!;
+
+    public string FacilityType { get; set; } = null!;
+
+    public int CountryId { get; set; }
+
+    public string? ZipPostalCode { get; set; }
+
+    public int CityId { get; set; }
+
+    public string? Street { get; set; }
+
+    public decimal? Latitude { get; set; }
+
+    public decimal? Longitude { get; set; }
+
+    public string? Remark { get; set; }
+
+    public int CreatedById { get; set; }
+
+    public DateTime CreatedOn { get; set; }
+
+    public int ModifiedById { get; set; }
+
+    public DateTime ModifiedOn { get; set; }
+
+    public int? ImportSequenceNo { get; set; }
+
+    public int SystemStatus { get; set; }
+
+    public virtual SmCity City { get; set; } = null!;
+
+    public virtual SmCountry Country { get; set; } = null!;
+
+    public virtual SmUser CreatedBy { get; set; } = null!;
+
+    public virtual SmUser ModifiedBy { get; set; } = null!;
+
+    public virtual ICollection<OghgEmployeeCommuting> OghgEmployeeCommutings { get; set; } = new List<OghgEmployeeCommuting>();
+
+    public virtual ICollection<OghgIndustrialProcess> OghgIndustrialProcesses { get; set; } = new List<OghgIndustrialProcess>();
+
+    public virtual ICollection<OghgMobileCombustion> OghgMobileCombustions { get; set; } = new List<OghgMobileCombustion>();
+
+    public virtual ICollection<OghgStationaryCombustion> OghgStationaryCombustions { get; set; } = new List<OghgStationaryCombustion>();
+
+    public virtual SmOrgGroup OrgGroup { get; set; } = null!;
+
+    public virtual BdcOrgUnit OrgUnit { get; set; } = null!;
+}
+
+
+public class BdcFacility2ReadDTOProfile : Profile
+{
+    public BdcFacility2ReadDTOProfile()
+    {
+        CreateMap<BdcFacility, BdcFacilityReadDTO>()
+        //.ForMember(dest => dest.CreateBy, opt => opt.MapFrom(src => src.CreateBy))
+        .ForMember(dest => dest.ModifiedBy, opt => opt.MapFrom(src => src.ModifiedBy))
+        .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+    }
+}
